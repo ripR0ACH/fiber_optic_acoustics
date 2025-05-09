77846a84bdce47706f50e55d30e652ac453f3ed8
ripR0ACH <morrisonizach@gmail.com>
2025-05-09 17:22:10 -0500

    really making some progress. Gotta figure out a few more things, then I'll have a working simulation lol!

--- .ipynb_checkpoints/fiber-checkpoint.py
+++ .ipynb_checkpoints/fiber-checkpoint.py
@@ -1,4 +1,3 @@
-from laser import Laser
 import numpy as np
 import matplotlib.pyplot as plt
 class Fiber:
@@ -21,20 +20,33 @@ class Fiber:
         self.fiber_ring = fr # distance of the center of the fiber from the center of the fiber bundle
         self.half = half
         self.i = i
-        self.center = self.set_pos()
-        self.x, self.y = self.draw_fiber()
+        
+        # This section defines the center of the fiber inside of the fiber bundle
+        # this is useful information for later when I need to draw the fibers
+        # or maybe not, I'll find out once I finish drafting.
+        # Since the fiber bundle will be axisymmetric, I need to define two separate
+        # halves of the bundle. Therefore the center of the first fiber is at the radius
+        # of the fiber and on the 0 y position.
+        # For the other half, the first fiber is at the opposite side of the x-axis,
+        # i.e. at x = r and y = 2r. 
+        # if self.theta == 0:
+        #     self.center = [self.r, 0] # the first fiber.
+        # elif self.theta < np.pi:
+        #     self.center = [self.r - self.fiber_ring * np.cos(self.theta), self.fiber_ring * np.sin(self.theta)] 
+        #     the rest of the fibers in the top half 
+        #     are drawn in arcs defined by concentric rings, fiber_ring.
+        #     therefore, depending on the ring and the angle theta, the fiber is set.
+        # elif self.theta == np.pi:
+        #     self.center = [self.r, 2 * self.r] # the first fiber on the other side of x-axis.
+        # else:
+        #     self.center = [self. r - self.fiber_ring * np.cos(self.theta), 2 * self.r - self.fiber_ring * np.cos(self.theta)]
+        #     same as above, the fibers are drawn in concentric rings defined by
+        #     by theta and the fiber_ring.
+        # self.center = [self.r * np.cos(self.theta) + self.fiber_ring, self.r * np.sin(self.theta) + self.fiber_ring]
+        self.pos, self.center = self.draw_fiber()
+        self.x, self.y = self.pos[0], self.pos[1]
         return
-    def set_pos(self):
-        if self.half == 0:
-            if self.i == 0:
-                return [self.r * np.cos(self.theta - np.pi / 2), self.r * np.sin(self.theta - np.pi / 2)]
-            else:
-                return [self.fiber_ring * np.cos(self.theta - np.pi), self.fiber_ring * np.sin(self.theta - np.pi) - self.r]
-        else:
-            if self.i == 0:
-                return [self.r * np.cos(self.theta - np.pi / 2), self.r * np.sin(self.theta - np.pi / 2)]
-            else:
-                return [self.fiber_ring * np.cos(self.theta - np.pi), self.fiber_ring * np.sin(self.theta - np.pi) + self.r]
+    
     def draw_fiber(self): 
         """
         
@@ -43,14 +55,20 @@ class Fiber:
         :return: x and y arrays for the circumference of the fiber being plotted.
         
         """
+        r = self.r # radius of the fiber to be drawn
+        t = self.theta # theta position of the fiber within the bundle
+        fr = self.fiber_ring # radius away from the center of the fiber bundle
         ts = np.linspace(0, 2 * np.pi, 100) # thetas for drawing the individual fiber
-        return [self.center[0] + self.r * np.cos(ts), self.center[1] + self.r * np.sin(ts)]
-    def intensity(self, l = Laser()):
-        I = 0
-        for r in l.rays:
-            if self.r ** 2 > (r.x - self.center[0]) ** 2 + (r.y - self.center[1]) ** 2:
-                I += r.I
-        return I
+        if self.half == 0:
+            if self.i == 0:
+                return [r * np.cos(ts), r * np.sin(ts) - r], [r * np.cos(t - np.pi / 2), r * np.sin(t - np.pi / 2)]
+            else:
+                return [r * np.cos(ts) - fr * np.cos(t), r * np.sin(ts) - fr * np.sin(t) - r], [fr * np.cos(t - np.pi), fr * np.sin(t - np.pi) - r]
+        else:
+            if self.i == 0:
+                return [r * np.cos(ts), r * np.sin(ts) + r], [r * np.cos(t - np.pi / 2), r * np.sin(t - np.pi / 2)]
+            else:
+                return [r * np.cos(ts) - fr * np.cos(t), r * np.sin(ts) - fr * np.sin(t) + r], [fr * np.cos(t - np.pi), fr * np.sin(t - np.pi) + r]
 
 class FiberBundle:
     def __init__(self, r, fiber_r = 1):
@@ -73,7 +91,7 @@ class FiberBundle:
                 f = np.append(f, Fiber(self.fr, t, ring, i, 1))
             n += len(ts)
         return n, f
-    def plot(self, centers = False, ax = None, figsize = (10, 10), *args, **kwargs):
+    def draw_bundle(self, centers = False, ax = None, figsize = (10, 10), *args, **kwargs):
         if ax == None:
             fig, ax = plt.subplots(1, 1, figsize = figsize)
         else:
@@ -89,8 +107,3 @@ class FiberBundle:
         ax.axline((-self.r, -self.r), (self.r, -self.r))
         ax.axline((-self.r, -self.r), (-self.r, self.r))
         return fig, ax
-    def sum_intensity(self, laser = Laser()):
-        I = 0
-        for f in self.fibers:
-            I += f.intensity(laser)
-        return I
--- laser.py
+++ laser.py
@@ -1,10 +1,23 @@
+import numpy as np
 class Ray:
-    def __init__(self):
+    def __init__(self, x = 0, y = 0, I = 0):
+        self.x = x
+        self.y = y
+        self.I = I
         return
-    def trace(self, dt):
+class Laser:
+    def __init__(self, rays = None):
+        if rays != None:
+            self.rays = np.array(rays)
+        else:
+            self.rays = self.make_gaussian_rays()
         return
-    def collide(self):
-        return
-    def reflect(self):
-        return
-    def normal(self):
+    def insert_point(self, point = Ray()):
+        return None
+    def append_point(self, point = Ray()):
+        self.rays = np.append(self.rays, point)
+        return None
+    def delete_point(self, ind = 0):
+        return None
+    def make_gaussian_rays(self):
+        return None
